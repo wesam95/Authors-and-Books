c62ce4aac04c2548c54ad8c949f29b80179bda11
wesam95 <wesamtakrouri@gmail.com>
2018-01-29 11:11:48 +0200

    removing old files

--- Authers_delete.php
+++
@@ -1,8 +0,0 @@
-<?php include_once './Classes/DB.php';
- include_once './Classes/Authers.php';
- 
-$id = $_GET['d'];
-$q = $authers->fetchdata("ID = '$id'");
-$q2 = $authers->delete("ID = '$id'");
-echo json_encode($q);
-	?>
--- book_form_fill.php
+++
@@ -1,7 +0,0 @@
-<?php include_once './Classes/DB.php';
-include_once './Classes/Books.php';
-
-$id = $_GET['d'];
- $res = $book->fetchdata('id = '.$id.' ');
-  echo json_encode($res);
-?>
